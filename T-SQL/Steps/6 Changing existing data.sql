886a909ea5d38dfc6b55042205bb1d916bc8f060
Nicklas Millard <nmillard@deloitte.dk>
2018-09-23 15:10:52 +0200

    add more to no. 6

--- T-SQL/Steps/6 Changing existing data.sql
+++ T-SQL/Steps/6 Changing existing data.sql
@@ -3,9 +3,36 @@
 
  /* Changing existing data
   * ------------------------------------------
-  * 
+  * Remember WHERE clauses. Without WHERE, all rows
+  * will get updated/deleted
   */
 
-  UPDATE Users
-  SET Username = 'Jimmy Dudeson'
-  WHERE Username = 'Tim Burry'
+  -- Update specific User's username
+  BEGIN TRANSACTION
+	  UPDATE dbo.Users
+	  SET Username = 'Tim Burry'
+	  WHERE Username = 'Jimmy Dudeso';
+
+	  IF @@ROWCOUNT != 1
+		PRINT 'No row updated.'
+  COMMIT
+
+  -- Delete user
+  BEGIN TRANSACTION
+	DECLARE @UserToDeleteById INT;
+
+	-- 1. Get user ID of the user that needs to be deleted
+	SET @UserToDeleteById = (SELECT Id
+							 FROM Users
+							 WHERE Username = 'Tom Dunckleberry');
+
+	-- 2. Delete the user from users-roles table
+	DELETE FROM UsersRoles
+	WHERE UserId = @UserToDeleteById;
+	
+	-- 3. then delete user from users table
+	DELETE FROM Users
+	WHERE Id = @UserToDeleteById;
+
+	-- SELECT * FROM dbo.Users; -- Uncomment to see result of transaction before rollback is called
+  ROLLBACK;
